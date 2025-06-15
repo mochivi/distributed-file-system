8d442fcfab8a6e7803ee2718aa89d59e4d1fda3b
mochivi <victormochi1@gmail.com>
2025-06-15 16:51:12 -0300

    parallelize tests, add extra test cases

--- dfs/tests/integration/upload_test.go
+++ dfs/tests/integration/upload_test.go
@@ -2,8 +2,10 @@ package integration
 
 import (
 	"context"
+	"math/rand"
 	"os"
 	"path/filepath"
+	"strconv"
 	"testing"
 	"time"
 
@@ -41,40 +43,41 @@ func TestClientUpload(t *testing.T) {
 
 	// Test inputs
 	testFilesDir := utils.GetEnvString("TEST_FILES_DIR", "/app/test-files")
-	uploadAt := "/user/files"
-	// defaultChunkSize := 8 * 1024 * 1024 // Default chunk size 8MB
+	baseUploadAt := "/user/files"
+	defaultChunkSize := 8 * 1024 * 1024 // Default chunk size 8MB
 
 	// Wait for datanodes states to be known by all datanodes -- heartbeat every 30s
 	// Temporary solution
 	time.Sleep(35 * time.Second)
 
-	// // Test varying file sizes
-	// fileSizeTestCases := []struct {
-	// 	name     string
-	// 	filepath string
-	// }{
-	// 	{name: "file size tests - 1MB", filepath: "small_test.txt"},
-	// 	{name: "file size tests - 10MB", filepath: "medium_test.txt"},
-	// 	{name: "file size tests - 100MB", filepath: "large_test.txt"},
-	// }
+	// Test varying file sizes
+	fileSizeTestCases := []struct {
+		name     string
+		filepath string
+	}{
+		{name: "file size tests - 1MB", filepath: "small_test.txt"},
+		{name: "file size tests - 10MB", filepath: "medium_test.txt"},
+		{name: "file size tests - 100MB", filepath: "large_test.txt"},
+	}
 
-	// for _, tt := range fileSizeTestCases {
-	// 	t.Run(tt.name, func(t *testing.T) {
-	// 		t.Parallel()
+	for _, tt := range fileSizeTestCases {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 
-	// 		path := filepath.Join(testFilesDir, tt.filepath)
-	// 		file, err := os.Open(path)
-	// 		if err != nil {
-	// 			t.Fatalf("failed to open file: %v", err)
-	// 		}
+			path := filepath.Join(testFilesDir, tt.filepath)
+			file, err := os.Open(path)
+			if err != nil {
+				t.Fatalf("failed to open file: %v", err)
+			}
 
-	// 		if err := client.UploadFile(context.Background(), file, uploadAt, defaultChunkSize); err != nil {
-	// 			t.Errorf("failed to upload file: %v", err)
-	// 		}
+			uploadAt := filepath.Join(baseUploadAt, tt.filepath, strconv.Itoa(rand.Intn(1000000)))
+			if err := client.UploadFile(context.Background(), file, uploadAt, defaultChunkSize); err != nil {
+				t.Errorf("failed to upload file: %v", err)
+			}
 
-	// 		file.Close()
-	// 	})
-	// }
+			file.Close()
+		})
+	}
 
 	// Test varying chunk sizes
 	chunkSizeTestCases := []struct {
@@ -82,13 +85,13 @@ func TestClientUpload(t *testing.T) {
 		filepath  string
 		chunkSize int
 	}{
-		// {name: "chunk size tests - 16MB", filepath: "large_test.txt", chunkSize: 16 * 1024 * 1024},
-		// {name: "chunk size tests - 32MB", filepath: "large_test.txt", chunkSize: 32 * 1024 * 1024},
+		{name: "chunk size tests - 16MB", filepath: "large_test.txt", chunkSize: 16 * 1024 * 1024},
+		{name: "chunk size tests - 32MB", filepath: "large_test.txt", chunkSize: 32 * 1024 * 1024},
 		{name: "chunk size tests - 64MB", filepath: "large_test.txt", chunkSize: 64 * 1024 * 1024},
-		// {name: "chunk size tests - 128MB", filepath: "large_test.txt", chunkSize: 128 * 1024 * 1024},
-		// {name: "chunk size tests - 256MB", filepath: "large_test.txt", chunkSize: 256 * 1024 * 1024},
-		// {name: "chunk size tests - 512MB", filepath: "large_test.txt", chunkSize: 512 * 1024 * 1024},
-		// {name: "chunk size tests - 1024MB", filepath: "large_test.txt", chunkSize: 1024 * 1024 * 1024},
+		{name: "chunk size tests - 128MB", filepath: "large_test.txt", chunkSize: 128 * 1024 * 1024},
+		{name: "chunk size tests - 256MB", filepath: "large_test.txt", chunkSize: 256 * 1024 * 1024},
+		{name: "chunk size tests - 512MB", filepath: "large_test.txt", chunkSize: 512 * 1024 * 1024},
+		{name: "chunk size tests - 1024MB", filepath: "large_test.txt", chunkSize: 1024 * 1024 * 1024},
 	}
 
 	for _, tt := range chunkSizeTestCases {
@@ -101,7 +104,8 @@ func TestClientUpload(t *testing.T) {
 				t.Fatalf("failed to open file: %v", err)
 			}
 
-			if err := client.UploadFile(context.Background(), file, filepath.Join(uploadAt, tt.filepath), tt.chunkSize); err != nil {
+			uploadAt := filepath.Join(baseUploadAt, tt.filepath, strconv.Itoa(rand.Intn(1000000)))
+			if err := client.UploadFile(context.Background(), file, uploadAt, tt.chunkSize); err != nil {
 				t.Errorf("failed to upload file: %v", err)
 			}
 
